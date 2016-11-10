162055afd6d963279145d5d4c1efcc17697766f2
ryanm@localhost <millwasx@gmail.com>
2016-11-10 15:05:02 -0500

    Basic Build

---
+++ Project_X/Assets/GameMaster.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameMaster : MonoBehaviour {
+
+    public static GameMaster gm;
+
+    void Start(){
+        if (gm == null){
+            gm = GameObject.FindGameObjectWithTag("GM").GetComponent<GameMaster>();
+        }
+    }
+
+    public Transform playerPrefab;
+    public Transform spawnPoint;
+    public int spawnDelay = 2;
+
+    public IEnumerator RespawnPlayer(){
+        yield return new WaitForSeconds(spawnDelay);
+        Instantiate(playerPrefab, spawnPoint.position, spawnPoint.rotation);
+    }
+
+	public static void KillPlayer(Stats player){
+        Destroy(player.gameObject);
+        gm.StartCoroutine(gm.RespawnPlayer());
+    }
+}
---
+++ Project_X/Assets/Stats.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Stats : MonoBehaviour {
+    [System.Serializable]
+    public class PlayerStats{
+        public int Health = 100;
+    }
+
+    public PlayerStats playerStats = new PlayerStats();
+
+    public int fallBoundary = -20;
+
+    void Update(){
+        if (transform.position.y <= fallBoundary)
+            DamagePlayer(999);
+    }
+
+    public void DamagePlayer(int damage){
+        playerStats.Health -= damage;
+        if(playerStats.Health <= 0){
+            GameMaster.KillPlayer(this);
+        }
+    }
+
+}
